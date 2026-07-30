f5a345de7a00a554f864e9871e0114205eb68ba4
HARIPRIYAN B A <118666047+Dany-6@users.noreply.github.com>
2026-07-30 14:38:00 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,26 +1,27 @@
+# Cyber Threat Classification Program
+
+# Sample network events
 network_events = [
-    "multiple failed login attempts",
-    "high traffic volume",
-    "malicious attachment detected",
-    "fake email requesting credentials",
-    "normal web browsing"
+    "multiple failed login attempts",
+    "high traffic volume",
+    "malicious attachment detected",
+    "fake email requesting credentials",
+    "normal web browsing"
 ]
 
 # Threat classification
 for event in network_events:
-    if "failed login" in event:
-        threat = "Unauthorized Access"
-    elif "high traffic" in event:
-        threat = "Denial of Service (DoS)"
-    elif "malicious attachment" in event:
-        threat = "Malware"
-    elif "fake email" in event:
-        threat = "Phishing"
-    else:
-        threat = "Normal Traffic"
+    if "failed login" in event:
+        threat = "Unauthorized Access"
+    elif "high traffic" in event:
+        threat = "Denial of Service (DoS)"
+    elif "malicious attachment" in event:
+        threat = "Malware"
+    elif "fake email" in event:
+        threat = "Phishing"
+    else:
+        threat = "Normal Traffic"
 
-    print("Network Event:", event)
-    print("Threat Category:", threat)
-    print()
-
-
+    print("Network Event:", event)
+    print("Threat Category:", threat)
+    print()
