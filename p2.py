c971a87646fcc4250381e97e622a96c1a1806507
HARIPRIYAN B A <118666047+Dany-6@users.noreply.github.com>
2026-07-30 09:14:19 +0530

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,45 @@
+web_events = [
+
+    "SQL query with user input",
+
+    "script tag detected in input field",
+
+    "multiple failed login attempts",
+
+    "file upload without validation",
+
+    "normal page request"
+
+]
+
+
+for event in web_events:
+
+    if "SQL query" in event:
+
+        threat = "SQL Injection Vulnerability"
+
+    elif "script tag" in event:
+
+        threat = "Cross Site Scripting (XSS)"
+
+    elif "failed login" in event:
+
+        threat = "Brute Force Attack"
+
+    elif "file upload" in event:
+
+        threat = "Unrestricted File Upload Vulnerability"
+
+    else:
+
+        threat = "No Threat Detected"
+
+
+    print("Event:", event)
+
+    print("Threat Identified:", threat)
+
+    print("-----------------------------")
+
+
